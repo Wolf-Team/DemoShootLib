1f29cea092bf9b1e1ef9b00b0f9db5dd2361756a
80LK <80LK@users.noreply.github.com>
2018-11-12 22:03:52 +0300

    Add files via upload

---
+++ dev/main.js
@@ -0,0 +1,203 @@
+IMPORT("ShootLib");
+
+var ShotType = ShootLib.ShotType;
+var ButtonType = ShootLib.ButtonType;
+
+ShootLib.init({
+	loadSoundFile:"EmptyGun.mp3",
+	crosshairGUI:{
+		bitmap:{
+			name:-1,
+			coords:{
+				x:0,
+				y:0,
+				width:2048,
+				height:512
+			},
+			size:{
+				width:4000,
+				height:1000
+			}
+		}
+	}
+});
+
+ShootLib.addGun({
+	id:"deserteagle",
+	name:"Desert Eagle",
+	ammo:"ammohandgun",
+	accuracy:6,
+	recoil:4,
+	rate:1,
+	texture:{
+		name:"deserteagle",
+		meta:0
+	},
+	shotType:ShotType.NORMAL,
+	buttonType:ButtonType.CLICK,
+	bullet:{
+		speed:10,
+		count:7,
+		damage:20
+	},
+	fov:{
+		level:10
+	},
+	sounds:{
+		shot:"DesertEagleShoot.ogg",
+		empty:"EmptyGun.mp3",
+		reload:"reload/LugerReload.ogg"
+	}
+});
+
+ShootLib.addGun({
+	id:"r870",
+	name:"R870",
+	ammo:"ammoshotgun",
+	accuracy:11,
+	recoil:26,
+	rate:15,
+	texture:{
+		name:"r870",
+		meta:0
+	},
+	shotType:ShotType.SHOTGUN,
+	buttonType:ButtonType.CLICK,
+	bullet:{
+		speed:10,
+		count:7,
+		damage:5
+	},
+	fov:{
+		level:10
+	},
+	sounds:{
+		shot:"R870Shoot.ogg",
+		empty:"EmptyGun.mp3",
+		reload:"reload/W1200Reload.ogg"
+	},
+	shotgun:{
+		count:8,
+		degreesSpread:3,
+	}
+});
+
+ShootLib.addGun({
+	id:"ak47",
+	name:"AK-47",
+	ammo:"ammoassault",
+	accuracy:5,
+	recoil:3,
+	rate:3,
+	texture:{
+		name:"ak47",
+		meta:0
+	},
+	shotType:ShotType.NORMAL,
+	buttonType:ButtonType.TOUCH,
+	bullet:{
+		speed:10,
+		count:30,
+		damage:20
+	},
+	fov:{
+		level:10
+	},
+	sounds:{
+		shot:"AK47Shoot.ogg",
+		empty:"EmptyGun.mp3",
+		reload:"reload/MP44Reload.ogg"
+	}
+});
+
+ShootLib.addGun({
+	id:"aa12",
+	name:"AA-12",
+	ammo:"ammoshotgun",
+	recoil:10,
+	rate:5,
+	texture:{
+		name:"aa12",
+		meta:0
+	},
+	shotType:ShotType.SHOTGUN,
+	buttonType:ButtonType.TOUCH,
+	accuracy:10,
+	bullet:{
+		speed:10,
+		count:8,
+		damage:5
+	},
+	fov:{
+		level:10
+	},
+	sounds:{
+		shot:"AA-12Shoot.ogg",
+		empty:"EmptyGun.mp3",
+		reload:"reload/PPSHReload.ogg"
+	},
+	shotgun:{
+		count:6,
+		degreesSpread:3,
+	}
+});
+
+ShootLib.addGun({
+	id:"barrett",
+	name:"Barrett",
+	ammo:"ammosniper",
+	accuracy:2,
+	recoil:25,
+	rate:12,
+	texture:{
+		name:"barrett",
+		meta:0
+	},
+	shotType:ShotType.NORMAL,
+	buttonType:ButtonType.CLICK,
+	bullet:{
+		speed:10,
+		count:10,
+		damage:20
+	},
+	fov:{
+		level:40,
+		link:"crosshair/dragunov"
+	},
+	sounds:{
+		shot:"BarrettShoot.ogg",
+		empty:"EmptyGun.mp3",
+		reload:"reload/BARReload.ogg"
+	}
+});
+
+
+ShootLib.addAmmos([{
+	id:"ammohandgun",
+	name:"Handgun Ammo",
+	texture:{
+		name:"ammohandgun",
+		meta:0
+	}
+},{
+	id:"ammoshotgun",
+	name:"Shotgun Ammo",
+	texture:{
+		name:"ammoshotgun",
+		meta:0
+	}
+},{
+	id:"ammoassault",
+	name:"Assault Rifle Ammo",
+	texture:{
+		name:"ammoassault",
+		meta:0
+	}
+},{
+	id:"ammosniper",
+	name:"Sniper Rifle Ammo",
+	texture:{
+		name:"ammosniper",
+		meta:0
+	}
+},]);
